4023af3ea7ed6fbfb14707775e241eded89240cf
Yelaman Fazyl <44122015+yelamanfazyl@users.noreply.github.com>
2023-06-13 00:28:18 +0600

    added mongo to deploy

--- app/config.py
+++ app/config.py
@@ -14,6 +14,7 @@ class Config(BaseSettings):
     MONGOUSER: str = "root"
     MONGOPASSWORD: str = "password"
     MONGODATABASE: str = "fastapi"
+    MONGO_URL: str = ""
 
 
 # environmental variables
@@ -21,13 +22,17 @@ env = Config()
 
 # FastAPI configurations
 fastapi_config: dict[str, Any] = {
-    "title": "API",
+    "title": "API nfactorial",
 }
 
-# MongoDB connection
-client = MongoClient(
+mongo_url = (
     f"mongodb://{env.MONGOUSER}:{env.MONGOPASSWORD}@{env.MONGOHOST}:{env.MONGOPORT}/"
 )
+if env.MONGO_URL:
+    mongo_url = env.MONGO_URL
+
+# MongoDB connection
+client = MongoClient(mongo_url)
 
 # MongoDB database
 database = client[env.MONGODATABASE]
